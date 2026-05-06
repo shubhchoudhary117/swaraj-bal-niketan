990baf69a9d3396201b19578cfe8362465f6de62
shubham choudhary <shubhchoudhary0156@gmail.com>
2026-05-06 16:19:55 +0530

    add teachers section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes } from 'react-router-dom'
 import PublicRoutes from './routes/public/PublicRoutes'
+import ScrollToTop from './shared/common/ScrollTop/ScrollTop'
 
 const App = () => {
   return <>
     <BrowserRouter>
-        <PublicRoutes />
+      <ScrollToTop />
+      <PublicRoutes />
     </BrowserRouter>
   </>
 }
