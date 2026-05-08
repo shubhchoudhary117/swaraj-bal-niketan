2b09d6030d0efe63113be145ecb6b791aad5b8ed
shubham choudhary <shubhchoudhary0156@gmail.com>
2026-05-08 18:06:24 +0530

    add student dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Routes } from 'react-router-dom'
 import PublicRoutes from './routes/public/PublicRoutes'
 import ScrollToTop from './shared/common/ScrollTop/ScrollTop'
 import PrivateRoutes from './routes/private/PrivateRoutes'
 
 const App = () => {
+
+  useEffect(() => {
+    document.addEventListener('wheel', function (event) {
+      if (event.ctrlKey) {
+        event.preventDefault();
+      }
+    }, { passive: false });
+
+    document.addEventListener('keydown', function (event) {
+      if (event.ctrlKey && (event.key === '+' || event.key === '-' || event.key === '0')) {
+        event.preventDefault();
+      }
+    });
+
+    document.addEventListener('gesturestart', function (event) {
+      event.preventDefault();
+    });
+  }, []);
+
   return <>
     <BrowserRouter>
       <ScrollToTop />
       <PublicRoutes />
-      <PrivateRoutes/>
+      <PrivateRoutes />
     </BrowserRouter>
   </>
 }
